b2202366890912820fcca2c1499dbe55cb890d37
Naida Torres <torresnr@clarkson.edu>
2024-12-10 16:13:43 -0500

    bug fixed index + script

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // generating a random number
-let randomNumber = Math.floor(Math.random() * 1000 + 1);
+let randomNumber = Math.floor(Math.random() * 100 + 1);
 
 // intialize number of guesses
-let guess = 1;
-document.getElementById("submit").onclick = checkGuess();
+let guesses = 1;
+// document.getElementById("submitGuess").onclick = checkGuess();
 
 function checkGuess() {
     // get user inputted guess/number
     let userGuess = document.getElementById("guess").value;
     if (userGuess == randomNumber) {
-        alert("Correct! You guessed the number in" + guess + "guesses.");
-    } else if (userGuess > randomNumber) {
-        guess++;
-        alert("Your guess is larger than the answer. Try again!")
-    } else {
-        guess++;
-        alert("Your guess is smaller than the answer. Try again!")
+        alert("Correct! You guessed the number in " + guesses + " guesses.");
     }
-}
+    else if (userGuess > randomNumber) {
+        guesses++;
+        alert("Your guess is larger than the answer. Guess a smaller number. Try again!");
+    }
+    else {
+        guesses++;
+        alert("Your guess is smaller than the answer. Guess a larger number. Try again!");
+    }
+};
